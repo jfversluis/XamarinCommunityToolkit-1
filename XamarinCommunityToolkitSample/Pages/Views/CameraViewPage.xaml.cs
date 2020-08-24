2031f83f788e6b6ec2591aac34f7c9f9f0137654
Pedro Jesus <pedrojesus.cefet@gmail.com>
2020-08-24 20:20:41 -0300

    improve the sample page

--- XamarinCommunityToolkitSample/Pages/Views/CameraViewPage.xaml.cs
+++ XamarinCommunityToolkitSample/Pages/Views/CameraViewPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Xamarin.CommunityToolkit.Sample.Pages;
+using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.Sample.Pages.Views
@@ -10,6 +12,48 @@ namespace Xamarin.CommunityToolkit.Sample.Pages.Views
 		public CameraViewPage()
 		{
 			InitializeComponent();
+			cameraView.MediaCaptured += (_, e) =>
+			{
+				switch (cameraView.CaptureOptions)
+				{
+					default:
+					case CameraCaptureOptions.Default:
+					case CameraCaptureOptions.Photo:
+						testImage.IsVisible = true;
+						testImage.Source = e.Image;
+						buttonShot.Text = "Shot";
+						break;
+					case CameraCaptureOptions.Video:
+						testImage.IsVisible = false;
+						//testMediaElement.IsVisible = true;
+						//testMediaElement.Source = e.Video;
+						buttonShot.Text = "Start record";
+						break;
+				}
+			};
+		}
+
+		private void shot_Clicked(object sender, EventArgs e)
+		{
+			cameraView.Shutter();
+			buttonShot.Text = cameraView.CaptureOptions != CameraCaptureOptions.Video ? "Shot" : "Stop record";
+		}
+
+		private void zoomSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+		{
+			cameraView.Zoom = (float)e.NewValue;
+		}
+
+		private void isPhoto_Toggled(object sender, ToggledEventArgs e)
+		{
+			cameraView.CaptureOptions = e.Value ? CameraCaptureOptions.Video : CameraCaptureOptions.Photo;
+
+			buttonShot.Text = cameraView.CaptureOptions != CameraCaptureOptions.Video ? "Shot" : "Stop record";
+		}
+
+		private void flash_Toggled(object sender, ToggledEventArgs e)
+		{
+			cameraView.FlashMode = e.Value ? CameraFlashMode.On : CameraFlashMode.Off;
 		}
 	}
 }
